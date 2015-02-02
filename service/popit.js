bc9da559288db5c2d1b7b7413b408f4d86f06ccf
Daniel Calligaro <dancalligaro@gmail.com>
2015-02-01 22:13:12 -0300

    uploading fileesss

--- service/popit.js
+++ service/popit.js
@@ -2,6 +2,9 @@ var Toolkit = require('popit-toolkit');
 var Q = require('q');
 var fs = require('fs');
 var mongoose = require('mongoose');
+var ssh2 = require('ssh2');
+var zlib = require('zlib');
+
 require('dotenv').load();
 
 if(!process.env.MONGO_DB_URL){
@@ -20,8 +23,11 @@ var CargoInstance = mongoose.model('CargoInstances', {
 	}
 });
 
-
 var currentBuilds = {};
+var buildQueue = [];
+var currentProcessing = null;
+
+createAndUploadIntance("lalala" + (+ new Date()), "cargografias");
 
 function createAndUploadIntance(instanceName, popitUrl){
 
@@ -69,51 +75,230 @@ function getBuildStatus(instanceName){
 	return deferred.promise;	
 }
 
+function getCurrentInstanceProgress(){
+	var deferred = Q.defer();
+	deferred.resolve( (currentProcessing && currentProcessing.progressLog) ? currentProcessing.progressLog : [] );
+	return deferred.promise;
+}
+
 function getAllInstances(){
 	var deferred = Q.defer();
 	deferred.resolve( CargoInstance.find() );
 	return deferred.promise;		
 }
 
+function getQueue(){
+	return buildQueue;
+}
+
 function addToBuildQueue(cargoInstance){
+	
 	if(currentBuilds[cargoInstance.name]){
-		console.log('error instance already scheduled for creation')
+		
+		console.log('error instance already scheduled')
+	
 	}else{
+
 		currentBuilds[cargoInstance.name] = cargoInstance;	
-
-		/*
-
-			toolkit = Toolkit({
-				host: popitUrl + ".popit.mysociety.org"
-			});
-
-		 	Q.all([
-		 		
-				toolkit.loadAllItems('persons'), 
-				toolkit.loadAllItems('organizations'),
-				toolkit.loadAllItems('posts'), 
-				toolkit.loadAllItems('memberships')
-			
-			]).spread(function(persons, organizations, posts, memberships){
-				
-				fs.writeFileSync('/vagrant/dump/persons.json', JSON.stringify(persons))
-				fs.writeFileSync('/vagrant/dump/organizations.json', JSON.stringify(organizations))
-				fs.writeFileSync('/vagrant/dump/posts.json', JSON.stringify(posts))
-				fs.writeFileSync('/vagrant/dump/memberships.json', JSON.stringify(memberships))
-
-				deferred.resolve();
-
-			}).catch(function(err){
-				deferred.reject(err);
-			});
-
-
-		*/
+		buildQueue.push(cargoInstance);	
+		processNext();
 
 	}
 }
 
+function processNext(){
+
+	if(!currentProcessing){
+		currentProcessing = buildQueue.shift();
+		if(currentProcessing){
+			processInstance(currentProcessing)
+				.then(function(){
+
+				})
+				.catch(function(){
+
+				})
+				.finally(function(){
+					setTimeout(processNext, 100);
+				});			
+		}
+	}
+
+}
+
+function processInstance(instance){
+
+	console.log('processing instance', instance.name)
+
+	var deferred = Q.defer();
+	var persons, organizations, posts, memberships;
+
+	toolkit = Toolkit({
+		host: instance.popitUrl + ".popit.mysociety.org"
+	});
+
+	instance.progressLog = [];
+	instance.progressLog.push('Loading Persons...')
+
+	toolkit.loadAllItems('persons').then(function(_persons){
+		console.log(_persons.length + ' persons loaded.')
+		instance.progressLog.push(_persons.length + ' persons loaded.')		
+		instance.progressLog.push('Loading Organizations...')		
+		persons = _persons;
+		return toolkit.loadAllItems('organizations');
+	}).progress(function(status){
+		console.log(status);
+	}).then(function(_organizations){
+		console.log(_organizations.length + ' organizations loaded.')
+		instance.progressLog.push(_organizations.length + ' organizations loaded.')		
+		instance.progressLog.push('Loading Posts...')		
+		organizations = _organizations;
+		return toolkit.loadAllItems('posts');
+	}).then(function(_posts){
+		console.log(_posts.length + ' posts loaded.')
+		instance.progressLog.push(_posts.length + ' posts loaded.')		
+		instance.progressLog.push('Loading Memberships...')		
+		posts = _posts;
+		return toolkit.loadAllItems('memberships');
+	}).then(function(_memberships){
+		console.log(_memberships.length + ' memberships loaded.')
+		instance.progressLog.push(_memberships.length + ' memberships loaded.')
+		memberships = _memberships;
+		console.log('ready to upload');
+		return uploadFilesToServer(instance.name, persons, organizations, posts, memberships);
+	}).progress(function(message){
+		instance.progressLog.push(message);
+	}).then(function(){
+		instance.progressLog.push('All files uploaded');
+	});
+
+	return deferred.promise;
+
+}
+
+function uploadFilesToServer(instanceName, persons, organizations, posts, memberships){
+	
+	var deferred = Q.defer();
+	var conn = new ssh2();
+	var files = [
+		{name: 'persons.json', file: persons}, 
+		{name: 'organizations.json', file: organizations},
+		{name: 'posts.json', file: posts},
+		{name: 'memberships.json', file: memberships}
+	];
+
+	conn.on(
+	    'connect',
+	    function () {
+	        console.log( "- connected" );
+	        deferred.notify('SSH connected')
+	    }
+	);
+	 
+	conn.on(
+	    'ready',
+	    function () {
+	        console.log( "- ready" );
+	 
+	        conn.sftp(
+	            function (err, sftp) {
+
+	                if ( err ) {
+	                    console.log( "Error, problem starting SFTP: %s", err );
+	                    deferred.reject("Error, problem starting SFTP: %s", err)
+	                }else{
+
+		 	            console.log( "- SFTP started" );
+		 	            deferred.notify("SFTP started");
+		 
+		                function uploadFile(file){
+	
+							zlib.gzip( JSON.stringify(file.file), function (error, result) {
+								
+								if(error){
+									console.log('- error gzipping file', file.name)
+									deferred.notify('- error gzipping file', file.name)
+								}else{
+
+					                var destinationPath = process.env.SSH_BASE_UPLOAD_PATH + "/" + instanceName + "-" + file.name
+					                var writeStream = sftp.createWriteStream( destinationPath );
+
+									console.log('uploading file ', file.name)
+									console.log('uploading to ', destinationPath);
+
+					                writeStream.write(result, function(){
+
+				                    	deferred.notify('upload complete ' + file.name)
+				                        console.log( "- file transferred " + file.name );
+				                        
+					                	if(files.length > 0){
+					                		setTimeout(function(){
+					                			uploadFile(files.shift());
+					                		},1)
+					                	}else{
+					                		deferred.notify('everything uploaded');
+					                		deferred.resolve();
+					                		sftp.end();
+					                	}
+
+					                });
+
+								}
+							});
+
+		                }
+
+		                uploadFile(files.shift());
+
+	                }
+	            }
+	        );
+	    }
+	);
+	 
+	conn.on(
+	    'error',
+	    function (err) {
+	        console.log( "- connection error: %s", err );
+	    }
+	);
+	 
+	conn.on(
+	    'end',
+	    function () {
+	        
+	    }
+	);
+
+	conn.connect(
+	    {
+	        "host": process.env.SSH_HOST,
+	        "port": 22,
+	        "username": process.env.SSH_USER,
+	        "password": process.env.SSH_PASSWORD
+	    }
+	);
+
+	return deferred.promise;
+};
+
+//Testing upload to server;
+
+/*
+
+	uploadFilesToServer('juanita', "['a', 'b']", "['a', 'b']", "['a', 'b']", "['a', 'b']").then(function(){
+		console.log("=====================================");
+		uploadFilesToServer('papapapa', "['a', 'b']", "['a', 'b']", "['a', 'b']", "['a', 'b']")
+	})
+
+*/
+
+
+
+
 module.exports = {
 	createAndUploadIntance: createAndUploadIntance, 
-	getAllInstances: getAllInstances
+	getAllInstances: getAllInstances, 
+	getCurrentInstanceProgress: getCurrentInstanceProgress
 };
+	
